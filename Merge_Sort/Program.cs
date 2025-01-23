de1d93c3643e79b5c0fb53db8353fe8a20606118
cipisek8 <cipisek@pecenka.name>
2025-01-23 09:33:25 +0100

    added int/string sorting to merge sort

--- Merge_Sort/Program.cs
+++ Merge_Sort/Program.cs
@@ -9,7 +9,7 @@ namespace Merge_Sort
             Console.WriteLine("Začínám třídit...");
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            SortUint("test_uint.txt");
+            SortString("test_words.txt");
             stopwatch.Stop();
             Console.WriteLine($"Třídil jsem {stopwatch.ElapsedMilliseconds} milisekund.");
 
@@ -39,7 +39,56 @@ namespace Merge_Sort
                 }
             }
         }
+        public static void SortInt(string file)
+        {
+            List<int> values = new();
 
+            using (StreamReader sr = new StreamReader("Inputs\\" + file))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    values.Add(int.Parse(line));
+                }
+            }
+
+            values = MergeSort(values);
+
+            using (StreamWriter sw = new StreamWriter("Outputs\\" + file))
+            {
+                for (int i = 0; i < values.Count - 1; i++)
+                {
+                    sw.WriteLine(values[i]);
+                }
+            }
+        }
+        public static void SortString(string file)
+        {
+            List<string> values = new();
+
+            using (StreamReader sr = new StreamReader("Inputs\\" + file))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    values.Add(line);
+                }
+            }
+
+            values = MergeSort(values);
+
+            using (StreamWriter sw = new StreamWriter("Outputs\\" + file))
+            {
+                for (int i = 0; i < values.Count - 1; i++)
+                {
+                    sw.WriteLine(values[i]);
+                }
+            }
+        }
         public static List<uint> MergeSort(List<uint> values)
         {
             if (values.Count <= 1)
@@ -67,6 +116,84 @@ namespace Merge_Sort
                 }
 
                 if (left[iLeft] <= right[iRight])
+                {
+                    tempValues.Add(left[iLeft]);
+                    iLeft++;
+                }
+                else
+                {
+                    tempValues.Add(right[iRight]);
+                    iRight++;
+                }
+            } while ((left.Count >= iLeft + 1) || (right.Count >= iRight + 1));
+            return tempValues;
+        }
+        public static List<int> MergeSort(List<int> values)
+        {
+            if (values.Count <= 1)
+                return values;
+
+            int middle = values.Count / 2;
+            List<int> left = MergeSort(values.Slice(0, middle).ToList());
+            List<int> right = MergeSort(values.Slice(middle, values.Count - middle).ToList());
+            int iLeft = 0;
+            int iRight = 0;
+            List<int> tempValues = new();
+            do
+            {
+                if (right.Count == iRight)
+                {
+                    tempValues.Add(left[iLeft]);
+                    iLeft++;
+                    continue;
+                }
+                if (left.Count == iLeft)
+                {
+                    tempValues.Add(right[iRight]);
+                    iRight++;
+                    continue;
+                }
+
+                if (left[iLeft] <= right[iRight])
+                {
+                    tempValues.Add(left[iLeft]);
+                    iLeft++;
+                }
+                else
+                {
+                    tempValues.Add(right[iRight]);
+                    iRight++;
+                }
+            } while ((left.Count >= iLeft + 1) || (right.Count >= iRight + 1));
+            return tempValues;
+        }
+        public static List<string> MergeSort(List<string> values)
+        {
+            if (values.Count <= 1)
+                return values;
+
+            int middle = values.Count / 2;
+            List<string> left = MergeSort(values.Slice(0, middle).ToList());
+            List<string> right = MergeSort(values.Slice(middle, values.Count - middle).ToList());
+            int iLeft = 0;
+            int iRight = 0;
+            List<string> tempValues = new();
+            do
+            {
+                if (right.Count == iRight)
+                {
+                    tempValues.Add(left[iLeft]);
+                    iLeft++;
+                    continue;
+                }
+                if (left.Count == iLeft)
+                {
+                    tempValues.Add(right[iRight]);
+                    iRight++;
+                    continue;
+                }
+
+                if (string.Compare(left[iLeft], right[iRight]) <= 0)
                 {
                     tempValues.Add(left[iLeft]);
                     iLeft++;
